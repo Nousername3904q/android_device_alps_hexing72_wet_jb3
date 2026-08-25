c911314963e13e1ba37015f0fdba9741e46c5963
cat <danielbenzahour202q@gmail.com>
2026-08-26 02:07:49 +0300

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_hexing72_wet_jb3.mk
+    $(LOCAL_DIR)/twrp_hexing72_wet_jb3.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_hexing72_wet_jb3-user \
-    omni_hexing72_wet_jb3-userdebug \
-    omni_hexing72_wet_jb3-eng
+    twrp_hexing72_wet_jb3-user \
+    twrp_hexing72_wet_jb3-userdebug \
+    twrp_hexing72_wet_jb3-eng
