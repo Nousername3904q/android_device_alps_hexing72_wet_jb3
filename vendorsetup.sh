971985e760191843e8561f3af0a4a0e2f00aed62
cat <danielbenzahour202q@gmail.com>
2026-08-26 02:08:11 +0300

    Update lunch combo entries for hexing72

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_hexing72_wet_jb3-user
-add_lunch_combo omni_hexing72_wet_jb3-userdebug
-add_lunch_combo omni_hexing72_wet_jb3-eng
+add_lunch_combo twrp_hexing72_wet_jb3-user
+add_lunch_combo twrp_hexing72_wet_jb3-userdebug
+add_lunch_combo twrp_hexing72_wet_jb3-eng
