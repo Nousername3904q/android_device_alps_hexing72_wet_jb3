c873caec16b04cf6ecb4f3248c1297eaaf88180b
Nousername3904q <danielbenzahour202q@gmail.com>
2026-08-26 01:30:44 +0300

    Initial device tree for hexing72_wet_jb3

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_hexing72_wet_jb3.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_hexing72_wet_jb3-user \
+    omni_hexing72_wet_jb3-userdebug \
+    omni_hexing72_wet_jb3-eng
---
+++ omni_hexing72_wet_jb3.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from hexing72_wet_jb3 device
+$(call inherit-product, device/alps/hexing72_wet_jb3/device.mk)
+
+PRODUCT_DEVICE := hexing72_wet_jb3
+PRODUCT_NAME := omni_hexing72_wet_jb3
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := T-5JX
+PRODUCT_MANUFACTURER := alps
+
+PRODUCT_GMS_CLIENTID_BASE := android-alps
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_hexing72_wet_jb3-userdebug 4.2.2 JDQ39 twrp-port test-keys"
+
+BUILD_FINGERPRINT := None
